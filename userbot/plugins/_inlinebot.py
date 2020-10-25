3fe31f08037970dd9e0992a90c24e1f193c12cfd
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-10-25 16:44:09 +0100

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -32,8 +32,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 buttons=buttons,
             )
             await event.answer([result] if result else None)
-        elif event.query.user_id == 713459844 and query.startswith("sudo"):
-        	
+    
         elif event.query.user_id == bot.uid and query.startswith("Userbot"):
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
