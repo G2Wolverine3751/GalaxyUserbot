1957c2fc5d30af5a121f8ea0de5e9e08570fb817
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-10-26 14:40:16 +0100

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -19,7 +19,7 @@ async def cmd_list(event):
         reply_to_id = event.reply_to_msg_id
     input_str = event.pattern_match.group(1)
     if input_str == "text":
-        string = "{count} comandi trovati in {plugincount} plugin di GalaxyUserbot"
+        string = "{count} comandi trovati in {plugincount} plugin di GalaxyUserbot\n\n"
         catcount = 0
         plugincount = 0
         for i in sorted(CMD_LIST):
@@ -63,10 +63,9 @@ async def cmd_list(event):
             await event.delete()
     else:
         if HELPTYPE is True:
-            help_string = f"Userbot di {DEFAULTUSER}\
-                          \nUserbot creato da ThePlayer372\
-                          \n__Do__ `.help` __plugin_name for commands, in case popup doesn't appear.__\
-                          \nDo `.info` plugin_name for usage"
+            help_string = f"Userbot creato da ThePlayer372\
+                          \n__Do__ `.help` __nome del plugin per ottenere i comandi.__\
+                          \nDo `.info` nome plugin per sapere l'utilizzo del plugin"
             tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
@@ -74,102 +73,15 @@ async def cmd_list(event):
             await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
             await event.delete()
         else:
-            string = "<b>Please specify which plugin do you want help for !!\
-                \nNumber of plugins : </b><code>{count}</code>\
-                \n<b>Usage:</b> <code>.help</code> plugin name\n\n"
+            string = "<b>Perfavore specifica il nome del plugin\
+                \nPlugin attivi: </b><code>{count}</code>\
+                \n<b>Usa:</b> <code>.help</code> nome plugin\n\n"
             catcount = 0
             for i in sorted(CMD_LIST):
                 string += "◆" + f"<code>{str(i)}</code>"
                 string += "   "
                 catcount += 1
             await event.edit(string.format(count=catcount), parse_mode="HTML")
-
-
-@bot.on(sudo_cmd(allow_sudo=True, pattern="help ?(.*)"))
-async def info(event):
-    input_str = event.pattern_match.group(1)
-    if input_str == "text":
-        string = "Total {count} commands found in {plugincount} sudo plugins of catuserbot\n\n"
-        catcount = 0
-        plugincount = 0
-        for i in sorted(SUDO_LIST):
-            plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
-            for iter_list in SUDO_LIST[i]:
-                string += "    " + str(iter_list)
-                string += "\n"
-                catcount += 1
-            string += "\n"
-        if len(string) > 4095:
-            data = string.format(count=catcount, plugincount=plugincount)
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": data}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the catuserbot are [here]({url})"
-            await event.reply(reply_text)
-            return
-        await event.reply(string.format(count=catcount, plugincount=plugincount))
-        return
-    if input_str:
-        if input_str in SUDO_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
-            catcount = 0
-            for i in SUDO_LIST[input_str]:
-                string += f"  ◆  <code>{i}</code>"
-                string += "\n"
-                catcount += 1
-            await event.reply(
-                string.format(count=catcount, input_str=input_str), parse_mode="HTML"
-            )
-        else:
-            reply = await event.reply(input_str + " is not a valid plugin!")
-            await asyncio.sleep(3)
-            await event.delete()
-            await reply.delete()
-    else:
-        string = "<b>Please specify which plugin do you want help for !!\
-            \nNumber of plugins : </b><code>{count}</code>\
-            \n<b>Usage:</b> <code>.help</code> plugin name\n\n"
-        catcount = 0
-        for i in sorted(SUDO_LIST):
-            string += "◆" + f"<code>{str(i)}</code>"
-            string += "   "
-            catcount += 1
-        await event.reply(string.format(count=catcount), parse_mode="HTML")
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
-@bot.on(sudo_cmd(pattern="info ?(.*)", allow_sudo=True))
-async def info(event):
-    """ For .info command,"""
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await edit_or_reply(event, str(CMD_HELP[args]))
-        else:
-            event = await edit_or_reply(event, "Please specify a valid plugin name.")
-            await asyncio.sleep(3)
-            await event.delete()
-    else:
-        string = "<b>Please specify which plugin do you want help for !!\
-            \nNumber of plugins : </b><code>{count}</code>\
-            \n<b>Usage : </b><code>.info</code> <plugin name>\n\n"
-        catcount = 0
-        for i in sorted(CMD_HELP):
-            string += "◆ " + f"<code>{str(i)}</code>"
-            string += "   "
-            catcount += 1
-        if event.from_id in Config.SUDO_USERS:
-            await event.reply(string.format(count=catcount), parse_mode="HTML")
-        else:
-            await event.edit(string.format(count=catcount), parse_mode="HTML")
-
 
 @bot.on(admin_cmd(outgoing=True, pattern="setinline (true|false)"))
 async def _(event):
@@ -181,13 +93,13 @@ async def _(event):
         type = False
     if HELPTYPE is True:
         if type is True:
-            await event.edit("`inline mode is already enabled`")
+            await event.edit("`Inline mode già attiva`")
         else:
             HELPTYPE = type
-            await event.edit("`inline mode is disabled`")
+            await event.edit("`Inline mode disattivata`")
     else:
         if type is True:
             HELPTYPE = type
-            await event.edit("`inline mode is enabled`")
+            await event.edit("`Inline mode attiva`")
         else:
-            await event.edit("`inline mode is already disabled`")
+            await event.edit("`Inline mode già disattiva`")
