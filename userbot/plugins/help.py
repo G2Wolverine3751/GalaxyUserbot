89c89b8e51c1675c3bcb02baa09bf7e4b5e63968
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-10-26 11:39:42 +0000

    pylint: auto fixes

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -4,7 +4,7 @@ import requests
 from telethon import functions
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd, yaml_format
-from . import ALIVE_NAME, CMD_HELP, CMD_LIST, SUDO_LIST, 
+from . import ALIVE_NAME, CMD_HELP, CMD_LIST, SUDO_LIST
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "galaxy"
 
