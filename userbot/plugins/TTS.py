80d75b81903bf9aa23ba74a06a2de7f43bf5fe65
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-11-28 15:00:01 +0100

    Create TTS.py

---
+++ userbot/plugins/TTS.py
@@ -0,0 +1,15 @@
+from ..utils import admin_cmd
+
+from .. import ALIVE_NAME
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "galaxy"
+
+@borg.on(admin_cmd(pattern="tts"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        reply = event.get_reply_message()
+        results = await bot.inline_query("t.me/TTSBot", reply)
+        await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+        await event.delete()        
