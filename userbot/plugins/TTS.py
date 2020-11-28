53d9a782ffde9771ebd05c4c09b214197d2e32b8
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-11-28 16:03:46 +0100

    Update TTS.py

--- userbot/plugins/TTS.py
+++ userbot/plugins/TTS.py
@@ -9,7 +9,10 @@ async def _(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
-        reply = event.get_reply_message()
-        results = await bot.inline_query("@TTSBot", reply)
-        await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
-        await event.delete()        
+        try:
+            reply = event.get_reply_message()
+            results = await bot.inline_query("TTSBot", reply)
+            await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+            await event.delete()
+        except Exception as e:
+            await event.delete(str(e))
