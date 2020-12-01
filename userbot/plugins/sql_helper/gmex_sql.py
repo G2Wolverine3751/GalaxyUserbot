1b5234a0a3c409373f52d0d56199ab8df2b4edcd
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-12-01 20:01:05 +0100

    Update gmex_sql.py

--- userbot/plugins/sql_helper/gmex_sql.py
+++ userbot/plugins/sql_helper/gmex_sql.py
@@ -15,13 +15,6 @@ class GMEX(BASE):
 
 GMEX.__table__.create(checkfirst=True)
 
-def get_all_gmex():
-    try:
-        return SESSION.query(GMEX).all()
-    except BaseException:
-        return None
-    finally:
-        SESSION.close()
 
 def is_gmex(chat_id, category):
     try:
@@ -31,6 +24,16 @@ def is_gmex(chat_id, category):
     finally:
         SESSION.close()
 
+
+def get_all_gmex():
+    try:
+        return SESSION.query(GMEX).all()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
 def addgmex(chat_id, category):
     adder = GMEX(str(chat_id), str(category))
     SESSION.add(adder)
@@ -38,7 +41,7 @@ def addgmex(chat_id, category):
 
 
 def removegmex(chat_id, category):
-    note = SESSION.query(ECHOSQL).get((str(chat_id), str(category)))
+    note = SESSION.query(GMEX).get((str(chat_id), str(category)))
     if note:
         SESSION.delete(note)
         SESSION.commit()
