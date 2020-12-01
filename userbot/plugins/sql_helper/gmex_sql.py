98196c6d71cb9ad7d4f40418fb683e6e9649dc5d
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-12-01 16:20:33 +0100

    Create gmex_sql.py

---
+++ userbot/plugins/sql_helper/gmex_sql.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, String
+
+from userbot.plugins.sql_helper import BASE, SESSION
+
+
+class GMEX(BASE):
+    __tablename__ = "gmex"
+    chat_id = Column(String(14), primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = str(chat_id)
+
+
+GMEX.__table__.create(checkfirst=True)
+
+def get_all_gmex():
+    try:
+        return SESSION.query(GMEX).all()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+def is_gmex(chat_id):
+    try:
+        return SESSION.query(GMEX).get(str(chat_id))
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+def addgmex(chat_id):
+    adder = GMEX(str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def removegmex(chat_id):
+    note = SESSION.query(GMEX).get(str(chat_id))
+    if note:
+        SESSION.delete(note)
+        SESSION.commit()
