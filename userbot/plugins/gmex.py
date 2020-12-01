6cae41be77d5d608b3e779ca28d47758194083f1
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-12-01 19:11:29 +0100

    Update gmex.py

--- userbot/plugins/gmex.py
+++ userbot/plugins/gmex.py
@@ -2,18 +2,19 @@ from .sql_helper.gmex_sql import get_all_gmex, addgmex, removegmex, is_gmex
 from ..utils import admin_cmd
 import asyncio
 
-@borg.on(admin_cmd(pattern="addgmex"))
+@borg.on(admin_cmd(pattern="addgmex (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     chat_id = event.chat_id
-    if is_gmex(chat_id):
-        await event.edit("Gruppo già aggiunto")
+    msg = event.pattern_match.group(1).strip().split(" ")[0]
+    if is_gmex(chat_id, msg):
+        await event.edit("Gruppo "+str(chat_id)+" già aggiunto nella lista "+msg)
         await asyncio.sleep(2)
         await event.delete()
     else:
-        addgmex(chat_id)
-        await event.edit("Gruppo "+str(chat_id)+" aggiunto con successo")
+        addgmex(chat_id, msg)
+        await event.edit("Gruppo "+str(chat_id)+" aggiunto con successo alla lista "+msg)
         await asyncio.sleep(2)
         await event.delete() 
 
@@ -23,22 +24,28 @@ async def _(event):
         return
     groups = get_all_gmex()
     msg = event.pattern_match.group(1)
+    category = event.pattern_match.group(1).strip().split(" ")[0]
+    msg = msg.replace(category, "").strip()
     await event.delete() 
     for x in groups:
-        await event.client.send_message(int(x.chat_id.strip()), msg)
+        cate = x.category.strip()
+        ids = int(x.chat_id.strip())
+        if str(cate) == str(category).strip():
+            await event.client.send_message(ids, msg)
 
-@borg.on(admin_cmd(pattern="rmgmex"))
+@borg.on(admin_cmd(pattern="rmgmex (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     chat_id = event.chat_id
+    msg = event.pattern_match.group(1).strip().split(" ")[0]
     if is_gmex(chat_id) == None:
-        await event.edit("Gruppo "+str(chat_id)+" mai aggiunto")
+        await event.edit("Gruppo "+str(chat_id)+" mai aggiunto nella lista "+msg)
         await asyncio.sleep(2)
         await event.delete()
     else:
-        removegmex(chat_id)
-        await event.edit("Gruppo "+str(chat_id)+" rimosso con successo")
+        removegmex(chat_id, msg)
+        await event.edit("Gruppo "+str(chat_id)+" rimosso con successo dalla lista "+msg)
         await asyncio.sleep(2)
         await event.delete()
     
