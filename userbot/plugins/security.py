4693a9458245a2d22263a8116cce38c0409f8031
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-12-03 13:00:54 +0100

    Update security.py

--- userbot/plugins/security.py
+++ userbot/plugins/security.py
@@ -22,20 +22,18 @@ async def _(event):
 @bot.on(events.NewMessage(incoming=True))
 async def onMessage(event):
     if event.sender_id == event.client.uid:
-        if is_approved(event.chat_id) == None:
-            approve(event.chat_id, "")
+        if is_approved(event.sender_id) == None:
+            approve(event.sender_id, "")
         return
     if not event.is_private:
         return
-    if is_approved(event.chat_id):
+    if is_approved(event.sender_id):
         return
     if is_in(event.sender_id):
         await event.delete()
         return
     chat_id = event.sender_id
-    add(chat_id)
-    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
-    results = await bot.inline_query(tgbotusername, "**Security")
-    await results[0].click(event.chat_id, hide_via=True)
+    add(event.sender_id)
+    await event.client.send_message(entity=chat_id,message="Ciao, aspetta che il mio capo ti approvi")
     
     
