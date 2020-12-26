9e12dcbe16f4e5f0379778d656c820033e4913ac
G2Wolverine3751 <64158371+G2Wolverine3751@users.noreply.github.com>
2020-12-26 12:43:14 +0100

    Create Info.py

---
+++ userbot/plugins/Info.py
@@ -0,0 +1,158 @@
+import html
+import os
+
+from requests import get
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+
+from ..utils import admin_cmd
+
+TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
+
+@bot.on(admin_cmd(pattern="info(?: |$)(.*)"))
+async def _(event):
+    cat = await event.edit("`Ottengo informazioni....`")
+    if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
+    replied_user = await get_user(event)
+    try:
+        photo, caption = await fetch_info(replied_user, event)
+    except AttributeError:
+        await event.edit("`Non ho trovato nessuna informazione.`")
+        return
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = None
+    try:
+        await event.client.send_file(
+            event.chat_id,
+            photo,
+            caption=caption,
+            link_preview=False,
+            force_document=False,
+            reply_to=message_id_to_reply,
+            parse_mode="html",
+        )
+        if not photo.startswith("http"):
+            os.remove(photo)
+        await cat.delete()
+    except TypeError:
+        await cat.edit(caption, parse_mode="html")
+
+
+async def get_user(event):
+    if event.reply_to_msg_id and not event.pattern_match.group(1):
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(
+            GetFullUserRequest(previous_message.sender_id)
+        )
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return replied_user
+
+
+async def fetch_info(replied_user, event):
+    replied_user_profile_photos = await event.client(
+        GetUserPhotosRequest(
+            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
+        )
+    )
+    replied_user_profile_photos_count = "L'utente non ha una foto profilo"
+    try:
+        replied_user_profile_photos_count = replied_user_profile_photos.count
+    except AttributeError:
+        pass
+    user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    last_name = replied_user.user.last_name
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
+    except:
+        dc_id = "Non posso ottenere il DC"
+    common_chat = replied_user.common_chats_count
+    username = replied_user.user.username
+    user_bio = replied_user.about
+    is_bot = replied_user.user.bot
+    restricted = replied_user.user.restricted
+    verified = replied_user.user.verified
+    photo = await event.client.download_profile_photo(
+        user_id, TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg", download_big=True
+    )
+    first_name = (
+        first_name.replace("\u2060", "")
+        if first_name
+        else ("L'utente non ha il primo nome")
+    )
+    last_name = last_name.replace("\u2060", "") if last_name else (" ")
+    username = "@{}".format(username) if username else ("Questo utente non ha un ursemane")
+    user_bio = "Questo utente non ha la bio" if not user_bio else user_bio
+    caption = "<b><i>Info di questo:</i></b>\n\n"
+    caption += f"<b>Nome e cognome:</b> <code>{first_name} {last_name}</code>\n"
+    caption += f"<b>Username:</b> <code>{username}</code>\n"
+    caption += f"<b> ID:</b> <code>{user_id}</code>\n"
+    caption += f"<b>DC id:</b> {dc_id}\n"
+    caption += f"<b>Numero di foto profilo:</b> {replied_user_profile_photos_count}\n"
+    caption += f"<b>Bio:</b> \n<code>{user_bio}</code>\n\n"
+    caption += f"<b>Chat in comune:</b> {common_chat}\n"
+    caption += f"<b>Perma link:</b> "
+    caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
+    return photo, caption
+
+async def get_user_from_event(event):
+    args = event.pattern_match.group(1).split(":", 1)
+    extra = None
+    if event.reply_to_msg_id and len(args) != 2:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif len(args[0]) > 0:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.edit("`Rispondi al messaggio di un utente`")
+            return
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        try:
+            user_obj = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_obj, extra
+
+
+async def ge(user, event):
+    if isinstance(user, str):
+        user = int(user)
+    try:
+        user_obj = await event.client.get_entity(user)
+    except (TypeError, ValueError) as err:
+        await event.edit(str(err))
+        return None
+    return user_obj
